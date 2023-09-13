26e78bf458d3b8a15a148f66c315c7c451474c7c
jdelg140 <jacobdelgado140@gmail.com>
2023-09-12 22:02:32 -0400

    re sctructured HTML/CSS and added a function to return a 5 day forcast

--- index.js
+++ index.js
@@ -1,10 +1,29 @@
 let apiKey="ca44d1ae7744258acf7128c94af7095f"
-let apiUrl="https://api.openweathermap.org/data/2.5/weather?"
+let apiUrl="https://api.openweathermap.org/data/2.5/weather?q=Miami"
+
+let forecastUrl="api.openweathermap.org/data/2.5/forecast?q=Miami"
 
 async function weatherCheck(){
-  let response = await fetch(apiUrl + `&appid=${apiKey}`)
+
+  let response = await fetch(apiUrl + `&appid=${apiKey}&units=imperial`)
   let data =await response.json()
   console.log(data)
+  document.querySelector('.tempnow').innerHTML = "Temperature: " + data.main.temp + " °F"
+  document.querySelector('.windnow').innerHTML ="Wind Speed: "+ data.wind.speed + " MPH"
+  document.querySelector('.humiditynow').innerHTML ="Humidity: "+ data.main.humidity + " %"
+  return data
+}
+
+async function forecast(){
+
+  let response = await fetch(forecastUrl + `&appid=${apiKey}&units=imperial`)
+  let data =await response.json()
+  console.log(data)
+  document.querySelector('#').innerHTML = 
+  document.querySelector('#').innerHTML = "Temperature: " + data.main.temp + " °F"
+  document.querySelector('#').innerHTML ="Wind Speed: "+ data.wind.speed + " MPH"
+  document.querySelector('#').innerHTML ="Humidity: "+ data.main.humidity + " %"
+  return data
 }
 
 weatherCheck()
