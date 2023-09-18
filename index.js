20c80e1f2ebd5f2330d2291ab2bad39cc5a20785
jdelg140 <jacobdelgado140@gmail.com>
2023-09-18 01:42:01 -0400

    added variables for button and user input, also expanded forecast function to display weather data for 5 days

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 let apiKey="ca44d1ae7744258acf7128c94af7095f"
-let apiUrl="https://api.openweathermap.org/data/2.5/weather?q=Miami"
+let apiUrl="https://api.openweathermap.org/data/2.5/weather?q="
 
-let forecastUrl="api.openweathermap.org/data/2.5/forecast?q=Miami"
+let forecastUrl="api.openweathermap.org/data/2.5/forecast?q="
+const searchBtn = document.querySelector(".search-btn")
+const city= document.querySelector(".city-input")
 
 async function weatherCheck(){
-
-  let response = await fetch(apiUrl + `&appid=${apiKey}&units=imperial`)
+  let response = await fetch(apiUrl + `${city.value}` + `&appid=${apiKey}&units=imperial`)
   let data =await response.json()
   console.log(data)
   document.querySelector('.tempnow').innerHTML = "Temperature: " + data.main.temp + " °F"
@@ -16,15 +17,43 @@ async function weatherCheck(){
 
 async function forecast(){
 
-  let response = await fetch(forecastUrl + `&appid=${apiKey}&units=imperial`)
-  let data =await response.json()
-  console.log(data)
-  document.querySelector('#').innerHTML = 
-  document.querySelector('#').innerHTML = "Temperature: " + data.main.temp + " °F"
-  document.querySelector('#').innerHTML ="Wind Speed: "+ data.wind.speed + " MPH"
-  document.querySelector('#').innerHTML ="Humidity: "+ data.main.humidity + " %"
+  let response = await fetch(forecastUrl + `${city.value}` + `&appid=${apiKey}&units=imperial`)
+  let dataF =await response.json()
+  console.log(dataF)
+  document.querySelector('#day1').innerHTML = 
+  document.querySelector('#temp1').innerHTML = "Temperature: " + dataF.list.temp + " °F"
+  document.querySelector('#wind1').innerHTML ="Wind Speed: "+ dataF.list.wind.speed + " MPH"
+  document.querySelector('#humidity1').innerHTML ="Humidity: "+ dataF.list.humidity + " %"
+
+  document.querySelector('#day2').innerHTML = 
+  document.querySelector('#temp2').innerHTML = "Temperature: " + dataF.list.temp + " °F"
+  document.querySelector('#wind2').innerHTML ="Wind Speed: "+ dataF.list.wind.speed + " MPH"
+  document.querySelector('#humidity2').innerHTML ="Humidity: "+ dataF.list.humidity + " %"
+
+  document.querySelector('#day3').innerHTML = 
+  document.querySelector('#temp3').innerHTML = "Temperature: " + dataF.list.temp + " °F"
+  document.querySelector('#wind3').innerHTML ="Wind Speed: "+ dataF.list.wind.speed + " MPH"
+  document.querySelector('#humidity3').innerHTML ="Humidity: "+ dataF.list.humidity + " %"
+
+  document.querySelector('#day4').innerHTML = 
+  document.querySelector('#temp4').innerHTML = "Temperature: " + dataF.list.temp + " °F"
+  document.querySelector('#wind4').innerHTML ="Wind Speed: "+ dataF.list.wind.speed + " MPH"
+  document.querySelector('#humidity4').innerHTML ="Humidity: "+ dataF.list.humidity + " %"
+
+  document.querySelector('#day5').innerHTML = 
+  document.querySelector('#temp5').innerHTML = "Temperature: " + dataF.list.temp + " °F"
+  document.querySelector('#wind5').innerHTML ="Wind Speed: "+ dataF.list.wind.speed + " MPH"
+  document.querySelector('#humidity5').innerHTML ="Humidity: "+ dataF.list.humidity + " %"
   return data
 }
 
-weatherCheck()
+searchBtn.addEventListener("click", () =>{
+  weatherCheck();
+  forecastUrl();
+ 
+})
+
+
+
+
 
